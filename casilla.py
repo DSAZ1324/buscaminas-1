4a2f2c2833595a76186ef716bcaf86f5eaa1ded5
egen0 <31622010+egen0@users.noreply.github.com>
2018-12-17 11:58:39 +0100

    Update casilla.py

--- casilla.py
+++ casilla.py
@@ -12,15 +12,15 @@ class Casilla:
         self.x = int(posx) #Posición de la casilla en el eje x
         self.y = int(posy) #Posición de la casilla en el eje y
         self.lev = False #Estado de la casilla, True = levantado, False = Tapado
-        self.val = None
+        self.val = random.randint(0,1)
     def get_pos(self):
         return self.x, self.y #Devuelve posición
     def get_lev(self):
        return self.lev#Devuelve el estado
     def set_lev(self, lev):
         self.lev = lev #Cambia de estado a la casilla
-    def set_val(self):  
-        self.val = random.randint(0,1)  #Devuelve entero entre 0 y 2, 
+    #def set_val(self):  
+     #   self.val = random.randint(0,1)  #Devuelve entero entre 0 y 2, 
 #0 = numero, 1 = mina.
     def get_val(self):  
         return self.val
