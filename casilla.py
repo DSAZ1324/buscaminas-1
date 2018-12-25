715997676822afafcbf6715316f4e6d96104a9b1
Eñaut <enautgenua@gmail.com>
2018-12-25 01:05:04 +0100

    añadido clase mina

--- casilla.py
+++ casilla.py
@@ -11,11 +11,8 @@ class Casilla:
         self.x = int(posx) #Posición de la casilla en el eje x
         self.y = int(posy) #Posición de la casilla en el eje y
         self.lev = False #Estado de la casilla, True = levantado, False = Tapado
-        #0 = numero, 1 = mina.
-        if random.randint(0,1) == 0:
-            self.val = numero(self.x, self.y)
-        else:
-            self.val = 1
+        self.val = random.randint(0, 1)#0 = numero, 1 = mina.
+     
     def get_pos(self):
         return self.x, self.y #Devuelve posición
     def get_lev(self):
@@ -39,7 +36,10 @@ class numero(Casilla):
     def get_valor(self):
         return self.contador       
 
-class mina(numero):
+class mina(Casilla):
     def __init__(self, posx, posy):
-        numero.__init__(self, posx, posy)
-        self.contador = 1
+        Casilla.__init__(self, posx, posy)
+        self.val = 1
+    def existe(self):
+        return self.val
+    
--- main.py
+++ main.py
@@ -2,14 +2,22 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Dec  2 21:49:00 2018
-
 @author: enaut
 """
 from casilla import Casilla
 from casilla import numero
+from casilla import mina
 from Tablero import Tablero
 matr = []
-def tablero(matr, x, y):
+def crear_minas_numeros(matr, x, y):
+    for i in range(int(x)):
+        for j in range(int(y)):
+            if matr[i][j] == 1:
+                matr[i][j] = mina(i, j)
+            else:
+                matr[i][j] = numero(i, j)
+    return matr
+def table(matr, x, y):
     lista = []
     listaux = []
     for i in range(int(x)):
@@ -19,23 +27,16 @@ def tablero(matr, x, y):
         lista.append(listaux)
         listaux = []
     return lista
-
 def actualizar_y_dibujar_tablero(lista, x, y):
     for i in range(int(x)):
         for j in range(int(y)):
             if lista[i][j] != 1:
                 if numero.get_lev(lista[i][j]):
                     None
-    
 dim = str(input("Introduzca las dimensiones de tu tablero (filas,columnas): "))
 (x, y) = dim.split(",")
 matr = Tablero(x, y)
 matr = Tablero.crear_tab(matr)
-a = tablero(matr, x, y)
-print(a)
-
-
-
-
-            
-    
+matr = table(matr, x, y)
+matr = crear_minas_numeros(matr, x, y)
+print(matr)
